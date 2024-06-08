e2e147069196431c1955baa6da34bd7e8954f4aa
sony-level <sonydilane@gmail.com>
2024-06-08 19:11:59 +0200

    add file to descrip projet

---
+++ src/app/components/About.tsx
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div className="about bg-[#0099FF] w-full h-fit text-white p-5 flex flex-col gap-4 overflow-hidden">
+      <h1 className="text-3xl font-bold">À PROPOS</h1>
+      <hr className="w-16 h-2 bg-white"></hr>
+      <p className="text-sm">
+        Bienvenue sur QuickMail, la destination en ligne ultime pour les personnes qui valorisent leur vie privée et recherchent des solutions de messagerie pratiques. Notre plateforme innovante vous permet de générer des adresses e-mail temporaires sans effort, vous offrant un moyen sûr et fiable de communiquer sans compromettre vos informations personnelles.
+      </p>
+      <p className="text-sm">
+        Grâce à notre interface conviviale, vous pouvez rapidement générer une adresse e-mail temporaire en quelques clics. Ces adresses e-mail sont entièrement fonctionnelles, vous permettant de recevoir des messages, des pièces jointes et des e-mails de vérification selon vos besoins. Votre boîte de réception temporaire est facilement accessible, vous permettant de vérifier les communications importantes tout en gardant votre compte e-mail principal dégagé et protégé des menaces potentielles.
+      </p>
+      <p className="text-sm">
+        Protégez votre vie privée et simplifiez votre communication en ligne avec QuickMail. Commencez à générer des adresses e-mail temporaires dès aujourd'hui et profitez d'un moyen sûr et sans tracas de participer à des activités en ligne tout en gardant vos informations personnelles sécurisées. Gardez le contrôle de votre présence numérique et profitez de la tranquillité d'esprit que vous méritez.
+      </p>
+    </div>
+  );
+};
+
+export default About;
